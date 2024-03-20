56dc8c45817e4c26b786440ae8ebdcf5ec024d79
Pierre GAMEN <pierre.gamen@gmail.com>
2024-03-20 14:20:08 +0100

    style (form) : Modifying the style of the Form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,14 +1,36 @@
+import { Divider } from 'semantic-ui-react'
 import './style.scss'
-export default function FormPage () {
+import { useState } from 'react'
+import FormMember from './FormMember'
+import FormOrganization from './FormOrganization'
+
+
+export default function FormPage() {
+    const [activeButton, setActiveButton] = useState('')
+
+    const handleClickMember = () => {
+        setActiveButton('member')
+    }
+
+    const handleClickOrganization = () => {
+        setActiveButton('organization')
+    }
+
     return (
-        <div className='section-card-formPage'>
-                <section className="section-card-formPage-card">
-                    <h2>Etudiant</h2>
-                </section>
-                <section className="section-card-formPage-card">
-                    <h2>Organisme de formation</h2>
-                </section>
-        </div>
+        <>
+            {!activeButton && (
+                <div className='section-card-formPage'>
+                    <section onClick={handleClickMember} className="section-card-formPage-card" id="member">
+                        <h2>Étudiant</h2>
+                    </section>
+                    <section onClick={handleClickOrganization} className="section-card-formPage-card" id="organization">
+                        <h2>Organisme de formation</h2>
+                    </section>
+                    </div>
+            )}
+            {activeButton === 'member' && (<div className="container-form"><FormMember /> <button onClick={()=> {setActiveButton('')}}>Retour vers le choix des profils</button></div>)}
+            {activeButton === 'organization' && (<div className="container-form"><FormOrganization /> <button onClick={()=> {setActiveButton('')}}>Retour vers le choix des profils</button></div>)}
         
+        </>
     )
 }
