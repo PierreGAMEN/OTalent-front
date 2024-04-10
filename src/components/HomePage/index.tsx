fcda2dabe279e9b7b190b92a44d70bdf467f998d
FlorianPERI <florian.peri@oclock.school>
2024-04-10 15:09:21 +0200

    fix(home):hero button + overflow

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,11 +1,7 @@
 import Hero from './Hero';
 import TrainingList from './TrainingList';
 import { useEffect, useState } from 'react';
-import {
-    fetchCategories,
-    fetchData,
-    requestWithVariable,
-} from '../../utils';
+import { fetchCategories, fetchData, requestWithVariable } from '../../utils';
 import {
     queryAllTrainingCard,
     queryCategories,
@@ -34,107 +30,132 @@ export default function HomePage() {
     const getTokenInformation = () => {
         if (user && user.member === true) {
             setIsMember(true);
-        } 
+        }
     };
-    
+
     const getMemberInformation = async () => {
-    
-            const variables = {
-                memberId: user.id
-            };
-            setIsMemberInfoLoaded(false);
-            const response = await requestWithVariable(queryMemberInformationForHomePage, variables);
-            setMemberInfo(response);
-            setIsMemberInfoLoaded(true);
+        const variables = {
+            memberId: user.id,
+        };
+        setIsMemberInfoLoaded(false);
+        const response = await requestWithVariable(
+            queryMemberInformationForHomePage,
+            variables
+        );
+        setMemberInfo(response);
+        setIsMemberInfoLoaded(true);
     };
 
     useEffect(() => {
         fetchData(queryAllTrainingCard, null, null, setData, setIsLoading);
-        fetchCategories();        
+        fetchCategories();
     }, []);
 
     useEffect(() => {
-        getTokenInformation()
-    }, [user])
+        getTokenInformation();
+    }, [user]);
 
     useEffect(() => {
-        if(isMember)
-        {setIsMemberInfoLoaded(false)
+        if (isMember) {
+            setIsMemberInfoLoaded(false);
             getMemberInformation().then(() => {
-                setIsMemberInfoLoaded(true)})}
-            
-    },[isMember] )
-;
- 
-        let count = 0
+                setIsMemberInfoLoaded(true);
+            });
+        }
+    }, [isMember]);
+
+    let count = 0;
     return (
         <main className="flex flex-col gap-20 mb-20">
             <Hero />
             <Feature />
             <Guide />
-
-    
-            {(isMemberInfoLoaded) ? (
-                <>
-                    {isMemberInfoLoaded && memberInfo.data.member.categories.length > 0 &&
+            <div id="training_list">
+                {isMemberInfoLoaded ? (
                     <>
-                    <h3 id="training_list">Vos catégories préférées</h3>
-                    {isLoading && <Loader active inline="centered" />}
-                    {memberInfo.data.member.categories.map(categorie => (
+                        {isMemberInfoLoaded &&
+                            memberInfo.data.member.categories.length > 0 && (
+                                <>
+                                    <h3>Vos catégories préférées</h3>
+                                    {isLoading && (
+                                        <Loader active inline="centered" />
+                                    )}
+                                    {memberInfo.data.member.categories.map(
+                                        categorie => (
+                                            <TrainingList
+                                                key={categorie.id}
+                                                data={data}
+                                                categoryChosen={categorie.label}
+                                            />
+                                        )
+                                    )}
+                                </>
+                            )}
+
+                        <h3>
+                            Proche de chez vous, en{' '}
+                            {memberInfo.data.member.region} :
+                        </h3>
+                        {isLoading && <Loader active inline="centered" />}
+                        <div className="flex overflow-x-auto overflow-y-hidden gap-5">
+                            {memberInfo.data.member.nearestOrganizations.map(
+                                organization =>
+                                    organization.trainings.map(training => {
+                                        if (count < 5) {
+                                            count++;
+                                            return (
+                                                <TrainingCard
+                                                    key={training.id}
+                                                    dateCreated={
+                                                        training.created_at
+                                                    }
+                                                    organizationId={
+                                                        training.organization.id
+                                                    }
+                                                    trainingId={training.id}
+                                                    label={training.label}
+                                                    duration={training.duration}
+                                                    price={training.price}
+                                                    organization={
+                                                        training.organization
+                                                            .name
+                                                    }
+                                                    category={
+                                                        training.category.label
+                                                    }
+                                                    image={training.image}
+                                                    categoryId={
+                                                        training.category.id
+                                                    }
+                                                    reviews={training.reviews}
+                                                />
+                                            );
+                                        } else {
+                                            return null;
+                                        }
+                                    })
+                            )}
+                        </div>
+                    </>
+                ) : (
+                    <Loader active inline="centered" />
+                )}
+
+                {isLoading && <Loader active inline="centered" />}
+
+                {data && (
+                    <>
+                        <h3>Découvrez notre sélection</h3>
                         <TrainingList
-                            key={categorie.id}
                             data={data}
-                            categoryChosen={categorie.label}
+                            categoryChosen="Informatique"
                         />
-                    ))}
-                    </>}
-
-                    <h3>Proche de chez vous, en {memberInfo.data.member.region} :</h3>
-                    {isLoading && <Loader active inline="centered" />}
-                    <div className='flex overflow-scroll gap-5'>
-                    {memberInfo.data.member.nearestOrganizations.map(organization => (
-                    organization.trainings.map(training => {
-                        if (count < 5) {
-                            count++;
-                            return (
-                                <TrainingCard
-                                    key={training.id}
-                                    dateCreated={training.created_at}
-                                    organizationId={training.organization.id}
-                                    trainingId={training.id}
-                                    label={training.label}
-                                    duration={training.duration}
-                                    price={training.price}
-                                    organization={training.organization.name}
-                                    category={training.category.label}
-                                    image={training.image}
-                                    categoryId={training.category.id}
-                                    reviews={training.reviews}
-                                />
-                            );
-                        } else {
-                            return null; 
-                        }
-                    })
-                        ))}
-                    </div>
-
-                </>
-            ) : (
-                <Loader active inline="centered" />
-            )}
-            
-            {isLoading && <Loader active inline="centered" />}
-            
-            {data && (
-                <>
-                    <h3>Découvrez notre sélection</h3>
-                    <TrainingList data={data} categoryChosen="Informatique" />
-                    <TrainingList data={data} categoryChosen="Arts" />
-                    <TrainingList data={data} categoryChosen="Finance" />
-                    <TrainingList data={data} categoryChosen="Business" />
-                </>
-            )}
+                        <TrainingList data={data} categoryChosen="Arts" />
+                        <TrainingList data={data} categoryChosen="Finance" />
+                        <TrainingList data={data} categoryChosen="Business" />
+                    </>
+                )}
+            </div>
         </main>
     );
 }
